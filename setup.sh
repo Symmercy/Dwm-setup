a759729db7da1023f4c71796b7ce590f9738dd0f
Symmercy <91673840+Symmercy@users.noreply.github.com>
2023-04-29 12:44:06 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,4 +6,14 @@ cd suckless
 git clone https://github.com/Symmercy/dwm
 cd dwm
 sudo make clean install
+sudo mkdir /usr/share/xsessions
+cd ~/Dwm-setup
+sudo mv dwm.desktop /usr/share/xsessions
+sudo systemctl enable lightdm
 
+echo "Do you want to reboot? (Y/N)"
+read $answer
+if [[ $answer == y ]]
+then
+  <reboot>
+fi
