46bf5b3255f3c1db13369801991b2898cad47cf4
Symmercy <91673840+Symmercy@users.noreply.github.com>
2023-04-29 15:51:46 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
-sudo pacman -S xorg lightdm lightdm-gtk-greeter dmenu ttf-font-awesome
+sudo pacman -S xorg lightdm lightdm-gtk-greeter ttf-font-awesome nitrogen
 mkdir suckless
 cd suckless
 git clone https://github.com/Symmercy/dwm
+git clone https://git.suckless.org/dmenu
 cd dwm
 sudo make clean install
+cd ..
+cd dmenu
+sudo make clean insall
 sudo mkdir /usr/share/xsessions
 cd ~/Dwm-setup
 sudo mv dwm.desktop /usr/share/xsessions
