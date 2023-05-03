4d86745c4c6d833538b88f2c7d666a490b865f02
Symmercy <91673840+Symmercy@users.noreply.github.com>
2023-05-03 12:31:08 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,6 +7,7 @@ cd suckless
 git clone https://github.com/Symmercy/dwm
 git clone https://git.suckless.org/dmenu
 git clone https://git.suckless.org/st
+git clone https://git.suckless.org/slstatus
 cd dwm
 sudo make clean install
 cd ..
@@ -15,10 +16,16 @@ sudo make clean install
 cd ..
 cd st
 sudo make clean install
+cd ..
+cd slstatus
+sudo make clean install
 sudo mkdir /usr/share/xsessions
 cd ~/dwm-setup
 sudo mv dwm.desktop /usr/share/xsessions
 sudo systemctl enable lightdm
+cd ~
+mkdir Utils
+cd Utils
 git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si
@@ -27,6 +34,8 @@ yay -S dracula-gtk-theme --noconfirm
 yay -S layan-cursor-theme-git --noconfirm
 cd ~
 touch .xprofile
-echo "nitrogen --restore" >> .xprofile
+echo "nitrogen --restore &" >> .xprofile
+echo "picom &" >> .xprofile
+echo "slstatus &" >> .xprofile
 echo "Installing and themeing DWM is finished, you can restart your computer"
 
