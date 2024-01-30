73bf3b91daea7bb14e3ba04027e1171dfc2ac882
Symmercy <91673840+Symmercy@users.noreply.github.com>
2024-01-30 19:21:11 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #Introducing
-echo"""                                                                                
+echo """                                                                                
                                                                                 
                                                         ,(((                    
              .                                         (####(      .            
